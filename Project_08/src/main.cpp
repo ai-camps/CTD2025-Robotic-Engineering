9b57b7e10006f5ac29768844e0102d416f77af9c
Jun Wen <jun.wen@ai-camps.com>
2025-07-21 17:30:03 -0500

    Update main.cpp

--- Project_08/src/main.cpp
+++ Project_08/src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
-// Device ID
-String deviceID;
-
 // Pin definitions
-constexpr auto BUTTON_PIN = 2;
+constexpr auto BUTTON_PIN = 4;
 
 void setup()
 {
+
   // Initialize serial communication at 115200 baud rate
   Serial.begin(115200);
-
-  // Get device ID
-  deviceID = String(ESP.getEfuseMac(), HEX);
 
   // Initialize the Red LED pin as an output
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -21,11 +16,8 @@ void setup()
 void loop()
 {
 
-  // Print the device ID
-  Serial.println(deviceID);
-
   // Read the state of the Red LED
-  Serial.println(digitalRead(BUTTON_PIN));
+  Serial.println(digitalRead(BUTTON_PIN) ? "Released" : "Pressed");
 
   delay(3000);
 }
