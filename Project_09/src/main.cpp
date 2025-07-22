4e28808b83aae178836b03336cf203d8fceae8b4
Jun Wen <jun.wen@ai-camps.com>
2025-07-21 21:14:04 -0500

    Update main.cpp

--- Project_09/src/main.cpp
+++ Project_09/src/main.cpp
@@ -1,8 +1,5 @@
 #include <Arduino.h>
 
-// Device ID
-String deviceID;
-
 // Pin definitions
 constexpr auto TILT_SENSOR_PIN = 2;
 
@@ -11,18 +8,12 @@ void setup()
   // Initialize serial communication at 115200 baud rate
   Serial.begin(115200);
 
-  // Get device ID
-  deviceID = String(ESP.getEfuseMac(), HEX);
-
   // Initialize the Red LED pin as an output
-  pinMode(TILT_SENSOR_PIN, INPUT);
+  pinMode(TILT_SENSOR_PIN, OUTPUT);
 }
 
 void loop()
 {
-
-  // Print the device ID
-  Serial.println(deviceID);
 
   // Read the state of the Red LED
   Serial.println(digitalRead(TILT_SENSOR_PIN));
